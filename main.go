86cc609746c6818653ee465a7c1d96df42106b1c
Glenn444 <gmark1586@gmail.com>
2025-04-22 07:24:04 +0300

    Stateful Handlers

--- main.go
+++ main.go
@@ -1,28 +1,54 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-type Handler interface {
-	ServeHTTP(http.ResponseWriter, *http.Request)
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
-type Server struct{
-	Address string
-	Handler Handler
+func Health(w http.ResponseWriter,req *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
-func Hello(w http.ResponseWriter,req *http.Request){
-	io.WriteString(w, "This is my website")
-}
-func main() {
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Reset Successfuly"))
+}
+
+
+func main() {
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
 	//rh := http.RedirectHandler("tobitresearchconsulting.com",307)
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/",fileServer)
+	fileServer := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/",cfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("/healthz",Health)
+	mux.HandleFunc("/metrics",cfg.MetricsHandler)
+	mux.HandleFunc("/reset",cfg.ResetHandler)
+
 
 	log.Print("Listening...")
 
