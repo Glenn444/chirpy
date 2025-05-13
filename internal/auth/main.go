71373fd6dd55da3301b403d4fb026d33174036fa
Glenn444 <gmark1586@gmail.com>
2025-05-13 21:59:27 +0300

    Jwt auth token

--- internal/auth/main.go
+++ internal/auth/main.go
@@ -1,8 +1,13 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -19,5 +24,45 @@ func HashPassword(password string)(string,error)  {
 func CheckPasswordHash(hash,password string)error  {
 	err := bcrypt.CompareHashAndPassword([]byte(hash),[]byte(password));
 return err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject: userID.String(),
+	});
+
+	signedToken,err := token.SignedString(tokenSecret);
+	if err != nil{
+		return "",err
+	}
+	return signedToken,nil
+}
+
+func ValidateJWT(tokenString,tokenSecret string)(uuid.UUID,error)  {
+	type MyCustomClaims struct {
+		jwt.RegisteredClaims
+	}
 	
+	token,err := jwt.ParseWithClaims(tokenString,&MyCustomClaims{},func(t *jwt.Token) (interface{}, error) {
+		return []byte("AllYourBase"),nil
+	});
+
+
+	if err != nil{
+		fmt.Printf("%v",err)
+		return uuid.Nil,err
+	}else if claims,ok := token.Claims.(*MyCustomClaims);ok{
+		fmt.Println(claims.RegisteredClaims.Subject)
+		uid,err := uuid.Parse(claims.RegisteredClaims.Subject)
+		if err != nil{
+			fmt.Printf("Error occurred converting string to uuid %v",err)
+		}
+		return uid,nil
+	}else{
+		log.Fatal("Unknown claims type,cannot proceed")
+		return uuid.Nil,errors.New("Unkown Claims")
+	}
 }
